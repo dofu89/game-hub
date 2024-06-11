370c6debaa07f1384c3f6f1a0c809b75c337648e
dofu89 <ethun89@gmail.com>
2024-06-11 23:28:04 +0200

    show loading skeletons

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -1,11 +1,4 @@
-import {
-  Card,
-  CardBody,
-  HStack,
-  Skeleton,
-  SkeletonText,
-  VStack,
-} from '@chakra-ui/react'
+import { Card, CardBody, Skeleton, SkeletonText } from '@chakra-ui/react'
 
 const GameCardSkeleton = () => {
   return (
